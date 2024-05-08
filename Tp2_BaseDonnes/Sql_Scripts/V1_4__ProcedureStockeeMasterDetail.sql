318ac4844f8e1459242bd2bc3aba5bd2d1334b3a
CC <2193774@cegepmontpetit.ca>
2024-05-08 15:45:35 -0400

    progres:procedure
    avancement

--- Tp2_BaseDonnes/Sql_Scripts/V1_4__ProcedureStockeeMasterDetail.sql
+++ Tp2_BaseDonnes/Sql_Scripts/V1_4__ProcedureStockeeMasterDetail.sql
@@ -1,31 +1,18 @@
-CREATE PROCEDURE GetDetailedButs
+CREATE PROCEDURE GetEquipesByDateRange
 @StartDate DATE,
-@EndDate DATE,
-@EquipeId INT
+@EndDate DATE
+
 AS
 BEGIN
-    SELECT
-        b.ButId,
-        b.DateBut,
-        b.MinuteBut,
-        b.descriptionBut,
-        j.JoueurId,
-        j.Nom AS JoueurNom,
-        j.Prenom AS JoueurPrenom,
-        e.EquipeId,
-        e.NomEquipe,
-        m.MatchId,
-        m.Date AS MatchDate,
-        m.Heure AS MatchHeure,
-        m.Duree AS MatchDuree
-    FROM
-        Matchs.But b
-    INNER JOIN Equipes.Joueur j ON b.JoueurID = j.JoueurId
-    INNER JOIN Contrats.ContratJoueur cj ON j.JoueurId = cj.JoueurID
-    INNER JOIN Equipes.Equipe e ON cj.EquipeId = e.EquipeId
-    INNER JOIN Matchs.Match1 m ON b.MatchId = m.MatchId
-    WHERE
-        m.Date BETWEEN @StartDate AND @EndDate
-        AND e.EquipeId = @EquipeId;
+    SELECT 
+        EquipeId, 
+        NomEquipe, 
+        Pays, 
+        CouleursEquipe, 
+        DateFondation
+    FROM 
+        Equipes.Equipe
+    WHERE 
+        DateFondation BETWEEN @StartDate AND @EndDate;
 END;
 GO
